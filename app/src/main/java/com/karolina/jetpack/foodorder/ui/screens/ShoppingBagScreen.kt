7670445d420e8cb409b13ab29cf66e3de7187901
Karolina Slemp-Rejowska <slempkarolina@gmail.com>
2024-04-10 09:55:29 +0200

    ShoppingBagScreen ui implementation

---
+++ app/src/main/java/com/karolina/jetpack/foodorder/ui/screens/ShoppingBagScreen.kt
@@ -0,0 +1,23 @@
+package com.karolina.jetpack.foodorder.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ShoppingBagScreen() {
+    Column() {
+        Text(text =)
+        ShoppingBagList()
+        SumUp()
+    }
+}
+
+@Composable
+fun SumUp() {
+
+}
+
+@Composable
+fun ShoppingBagList() {
+}
